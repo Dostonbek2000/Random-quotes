38f022ec8efbf576632830a7eddb9d548516be0f
Dostonbek2000 <simple2000code@gmail.com>
2022-01-05 20:47:01 -0800

    random app

---
+++ src/Dashboard/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div id="footer">
+      <span>by</span>
+      <a rel="noopener noreferrer" href="https://github.com/Dostonbek2000">
+        😺Dostonbek😺
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
